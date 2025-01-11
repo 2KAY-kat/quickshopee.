3a63ef12d195db66e9d4ab39f048b4c7499bdec3
2KAY-kat <dambwedesigns@gmail.com>
2025-01-11 21:36:00 +0200

    commited to be done

--- scripts/app.js
+++ scripts/app.js
@@ -91,3 +91,19 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+
+const backToTop = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        backToTop.style.display = 'block';
+    } else {
+        backToTop.style.display = 'none'
+    }
+});
+
+backToTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
