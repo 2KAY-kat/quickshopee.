f9ef7a92f3d75b4be6dbc80f004620eb4ced69f1
2KAY-kat <dambwedesigns@gmail.com>
2025-03-09 13:37:22 +0200

    new device new repo and assets

--- scripts/data.js
+++ scripts/data.js
@@ -22,6 +22,246 @@ export const products = [
         price: '$30',
         description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
         dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
+    },
+    {
+        id: 3,
+        name: 'Nuts',
+        image: 'assets/images/biscuitssnacks-1.jpg', 
+        price: '$30',
+        description: 'lorem some description text here The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        dataProductId: '3'
     }
 ];
 
